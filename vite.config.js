7b5dcf9e241df1c0be0bdca1f51c16276a5e98e3
Joerg Polkehn <jpolkehn@gmail.com>
2024-01-31 15:03:27 +0100

    init commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
-import { imagetools } from 'vite-imagetools';
-import htmlImages from 'vite-plugin-html-images';
-import handlebars from 'vite-plugin-handlebars';
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import { imagetools } from "vite-imagetools";
+import htmlImages from "vite-plugin-html-images";
+import handlebars from "vite-plugin-handlebars";
 
 module.exports = defineConfig({
   root: "src",
@@ -10,14 +10,19 @@ module.exports = defineConfig({
     outDir: "../dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/index.html'),
-        nested: resolve(__dirname, 'src/sub-page/index.html')
-      }
-    }
+        main: resolve(__dirname, "src/index.html"),
+        cafe: resolve(__dirname, "src/pages/cafe.html"),
+        contact: resolve(__dirname, "src/pages/contact.html"),
+        herkunft: resolve(__dirname, "src/pages/herkunft.html"),
+        impressum: resolve(__dirname, "src/pages/impressum.html"),
+        product: resolve(__dirname, "src/pages/product.html"),
+        shop: resolve(__dirname, "src/pages/shop.html"),
+      },
+    },
   },
   plugins: [
     imagetools(),
     htmlImages(),
-    handlebars({partialDirectory: resolve(__dirname, 'src/components'),})
-  ]
-})
+    handlebars({ partialDirectory: resolve(__dirname, "src/components") }),
+  ],
+});
