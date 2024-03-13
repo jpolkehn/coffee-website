37c2605374ef5455dc38ded7e1582b0c96a5f058
Joerg Polkehn <jpolkehn@gmail.com>
2024-03-13 13:02:28 +0100

    init commit

--- src/js/scrollNavbar.js
+++ src/js/scrollNavbar.js
@@ -1,23 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var navigation = document.getElementById("navigation");
-  var header = document.querySelector("header");
-  var logo = document.querySelector(".nav__logo img");
-  var links = document.querySelectorAll(".menu-link");
+  const navigation = document.getElementById("navigation");
+  const header = document.querySelector("header");
+  const logo = document.querySelector(".nav__logo img");
+  const links = document.querySelectorAll(".menu-link");
 
   window.addEventListener("scroll", function () {
     if (window.scrollY > 100) {
       navigation.classList.remove("nav--transparent");
       navigation.style.backgroundColor = "white";
       header.style.paddingTop = navigation.offsetHeight + "px";
-      for (var i = 0; i < links.length; i++) {
+      for (vonst i = 0; i < links.length; i++) {
         links[i].style.color = "black";
       }
       logo.src = "/Logo.svg";
-    } else {
+    }, else {
       navigation.classList.add("nav--transparent");
       navigation.style.backgroundColor = "transparent";
       header.style.paddingTop = "0";
-      for (var i = 0; i < links.length; i++) {
+      for (const i = 0; i < links.length; i++) {
         links[i].style.color = "white";
       }
       logo.src = "/images/icons/Logo.svg";
